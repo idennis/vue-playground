16b8a4a9c21001bc9f8f9bb241e6ba409a7be574
Dennis Hou <dennis@fillr.com>
2017-12-07 11:59:31 +1100

    New components

--- app.js
+++ app.js
@@ -9,12 +9,29 @@ window.onload = function () {
         { name: 'buy eggs' },
         { name: 'get prescription' },
         { name: 'meet mum' },
-      ]
+      ],
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ],
     },
     methods: {
       reverseMessage: function(){
         this.message = this.message.split('').reverse().join('');
       }
     },
+  },
+  
+  // Register a new Component
+  Vue.component('todo-item', {
+    // Props allow child component to reference
+    // parent component's data (vice versa)
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
   })
+)
+  
+
+  
 };
